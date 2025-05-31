690df27b9289c70d4538fe074ea1297326b3255d
李宁搏 <liningbo@liningbodeMacBook-Pro.local>
2025-05-31 02:41:04 -0700

    fix5

--- moodtune/src/app/api/auth/[...nextauth]/route.ts
+++ moodtune/src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
+console.log('ENV:', process.env);
+
 const prisma = new PrismaClient();
 
 const handler = NextAuth({
